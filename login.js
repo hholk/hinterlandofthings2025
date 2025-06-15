3fcff24b52d2ac63a90e76bd8422d6fbcb3c0785
hholk <henrikholkenbrink@gmail.com>
2025-06-15 18:45:24 +0200

    Scope login script to form

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 const form = document.querySelector('.login-form');
-if(form){
-  form.addEventListener('submit', e => {
+if (form) {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    if(email === 'henrikholkenbrink@gmail.com' && password === '1234asdf'){
+    const email = form.querySelector('#email').value;
+    const password = form.querySelector('#password').value;
+    if (email === 'henrikholkenbrink@gmail.com' && password === '1234asdf') {
       localStorage.setItem('loggedIn', 'true');
       window.location.href = '../index.html';
     } else {
