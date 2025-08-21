de68ed5a2f90300ef7328395a6e120ccf7470764
hholk <henrikholkenbrink@gmail.com>
2025-08-21 04:02:01 -0700

    test: avoid hourly cron pattern

---
+++ cron-validator.test.js
@@ -0,0 +1,18 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const fs = require('node:fs');
+const { isHobbyCronValid } = require('./cron-validator');
+
+test('hourly cron alias is invalid for Hobby plan', () => {
+  assert.strictEqual(isHobbyCronValid('@hourly'), false);
+});
+
+test('daily cron expression is valid for Hobby plan', () => {
+  assert.strictEqual(isHobbyCronValid('0 0 * * *'), true);
+});
+
+test('vercel.json schedule complies with Hobby plan', () => {
+  const config = JSON.parse(fs.readFileSync('./vercel.json', 'utf8'));
+  const schedule = config.crons[0].schedule;
+  assert.strictEqual(isHobbyCronValid(schedule), true);
+});
